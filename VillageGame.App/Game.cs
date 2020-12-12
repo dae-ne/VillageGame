fb55e9db5b2733b81a1b368e9c776f78d7901bd5
rarcz29 <rafalcz29@gmail.com>
2020-12-12 19:11:10 +0100

    Update textures

--- VillageGame.App/Game.cs
+++ VillageGame.App/Game.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.Window;
+using System;
 using System.Collections.Generic;
 using VillageGame.App.GameStates;
 
@@ -7,15 +8,20 @@ namespace VillageGame.App
 {
     class Game
     {
-        private List<IState> _states;
+        private List<IState> _states = new List<IState>();
         //private RenderWindow window;
+        private readonly TextureManager _textureManager = new TextureManager();
         private const string _WindowName = "VillageGame";
+
+        private Sprite sprite;
 
         public RenderWindow Window { get; private set; }
 
         public Game(uint resolutionH, uint resolutionV)
         {
+            LoadTextures();
             Window = new RenderWindow(new VideoMode(resolutionH, resolutionV), _WindowName);
+            Window.SetFramerateLimit(60);
         }
 
         public void GameLoop()
@@ -23,6 +29,18 @@ namespace VillageGame.App
             while (Window.IsOpen)
             {
 
+            }
+        }
+
+        private void LoadTextures()
+        {
+            try
+            {
+                _textureManager.LoadTexture("grass", "Images/Grass.png");
+            }
+            catch
+            {
+                Console.WriteLine("Cannot load textures");
             }
         }
     }
