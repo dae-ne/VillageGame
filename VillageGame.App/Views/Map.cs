aa714354ad74d1893834f6554ddfec580e77161e
rarcz29 <rafalcz29@gmail.com>
2020-12-12 19:42:52 +0100

    Add Map class

---
+++ VillageGame.App/Views/Map.cs
@@ -0,0 +1,21 @@
+﻿using SFML.Graphics;
+
+namespace VillageGame.App.Views
+{
+    class Map : Drawable
+    {
+        private int _height;
+        private int _width;
+
+        public Map()
+        {
+            _width = 0;
+            _height = 0;
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
