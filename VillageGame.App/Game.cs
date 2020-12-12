429465ed81b0d8ecad7c265ae4607c2deba9ed13
rarcz29 <rafalcz29@gmail.com>
2020-12-12 18:14:52 +0100

    Add IState interface and Game class

---
+++ VillageGame.App/Game.cs
@@ -0,0 +1,17 @@
+﻿using SFML.Graphics;
+using System.Collections.Generic;
+using VillageGame.App.GameStates;
+
+namespace VillageGame.App
+{
+    class Game
+    {
+        private List<IState> states;
+        private RenderWindow window;
+
+        public void GameLoop()
+        {
+
+        }
+    }
+}
---
+++ VillageGame.App/GameStates/IState.cs
@@ -0,0 +1,19 @@
+﻿namespace VillageGame.App.GameStates
+{
+    interface IState
+    {
+        //    public:
+
+        //Game* game;
+
+        //    virtual void draw(const float dt) = 0;
+        //    virtual void update(const float dt) = 0;
+        //    virtual void handleInput() = 0;
+
+        Game App { get; }
+
+        void Draw();
+        void Update();
+        void HandleInput();
+    }
+}
