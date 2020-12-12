6f4e48a3ba24dda63a1b1fdcc43ffb2b790c48e5
rarcz29 <rafalcz29@gmail.com>
2020-12-12 18:07:32 +0100

    Create new solution and add sfml project

---
+++ VillageGame.App/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace VillageGame.App
+{
+    class EntryPoint
+    {
+        static void OnClose(object sender, EventArgs e)
+        {
+            var window = (RenderWindow)sender;
+            window.Close();
+        }
+
+        static void Main()
+        {
+            var window = new RenderWindow(new VideoMode(800, 600), "SFML window");
+            window.Closed += new EventHandler(OnClose);
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear();
+                window.Display();
+            }
+        }
+    }
+}
