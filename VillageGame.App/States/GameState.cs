02a3d65feab6450e48194f995ae00975b38ea999
rarcz29 <rafalcz29@gmail.com>
2020-12-12 21:28:41 +0100

    Update game state

--- VillageGame.App/States/GameState.cs
+++ VillageGame.App/States/GameState.cs
@@ -1,17 +1,38 @@
-﻿namespace VillageGame.App.States
+﻿using SFML.Graphics;
+using SFML.System;
+using System.Collections.Generic;
+using VillageGame.App.Tiles;
+using VillageGame.App.Views;
+
+namespace VillageGame.App.States
 {
     class GameState : IState
     {
+        private View _gameView = new View();
+        private View _guiView = new View();
+        private Map _map = new Map();
+        private float _zoomLevel = 1.0f;
+
         public Game App { get; }
 
-        public GameState(Game game)
+        public GameState(Game game, Dictionary<string, ITile> tiles)
         {
             App = game;
+            var windowSizeUint = App.Window.Size;
+            var x = (float)windowSizeUint.X;
+            var y = (float)windowSizeUint.Y;
+            var windowSize = new Vector2f(x, y);
+            _gameView.Size = windowSize;
+            _guiView.Size = windowSize;
+            _ = _map.LoadFromFileAsync("Saves/empty.dat", tiles);
         }
 
         public void Draw()
         {
             App.Window.Clear();
+            //App.Window.SetView(_guiView);
+            App.Window.SetView(_gameView);
+            App.Window.Draw(_map);
         }
 
         public void HandleInput()
