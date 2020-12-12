c183ddfc16a23a15d79c5b0b927067418441edaf
rarcz29 <rafalcz29@gmail.com>
2020-12-12 18:25:33 +0100

    Add game and menu states

---
+++ VillageGame.App/GameStates/GameState.cs
@@ -0,0 +1,22 @@
+﻿namespace VillageGame.App.GameStates
+{
+    class GameState : IState
+    {
+        public Game App => throw new System.NotImplementedException();
+
+        public void Draw()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleInput()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VillageGame.App/GameStates/MenuState.cs
@@ -0,0 +1,22 @@
+﻿namespace VillageGame.App.GameStates
+{
+    class MenuState : IState
+    {
+        public Game App => throw new System.NotImplementedException();
+
+        public void Draw()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleInput()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
