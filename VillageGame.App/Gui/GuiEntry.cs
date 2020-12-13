0ed7e329f15a35a62a533af800af53d37d83eaa8
rarcz29 <rafalcz29@gmail.com>
2020-12-13 14:07:26 +0100

    Create GuiStyle and GuiEntry

---
+++ VillageGame.App/Gui/GuiEntry.cs
@@ -0,0 +1,14 @@
+﻿using SFML.Graphics;
+
+namespace VillageGame.App.Gui
+{
+    class GuiEntry : Drawable
+    {
+        private RectangleShape _rectangle = new RectangleShape();
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VillageGame.App/Gui/GuiStyle.cs
@@ -0,0 +1,35 @@
+﻿using SFML.Graphics;
+
+namespace VillageGame.App.Gui
+{
+    class GuiStyle
+    {
+        public Color BodyColor { get; set; }
+        public Color BodyHighlight { get; }
+        public Color BodyHighlighColor { get; set; }
+        public Color BorderColor { get; set; }
+        public Color BorderHighlightColor { get; set; }
+        public Color TextColor { get; set; }
+        public Color TextHighlightColor { get; set; }
+        public Font Font { get; set; }
+
+        public GuiStyle() { }
+
+        public GuiStyle(Font font,
+                        Color bodyColor,
+                        Color bodyHighlight,
+                        Color borderColor,
+                        Color borderHighlightColor,
+                        Color textColor,
+                        Color textHighlightColor)
+        {
+            Font = font;
+            BodyColor = bodyColor;
+            BodyHighlight = bodyHighlight;
+            BorderColor = borderColor;
+            BorderHighlightColor = borderHighlightColor;
+            TextColor = textColor;
+            TextHighlightColor = textHighlightColor;
+        }
+    }
+}
